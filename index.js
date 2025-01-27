2a0fe3236b33b369e7582495738d0017fecf6747
sakib-celloscope <sakib.celloscope@gmail.com>
2025-01-27 16:20:57 +0600

    fixed touch

--- index.js
+++ index.js
@@ -248,24 +248,20 @@ const show_floating_button = () => {
         const next_button = document.getElementById('next_button');
         const previous_button = document.getElementById('previous_button');
 
-        top_button.className = 'absolute rounded-full transition-all duration-[0.2s] ease-out scale-y-100 -translate-x-16 flex p-3 scale-100 bg-green-600 text-white cursor-pointer'
+        top_button.className = 'absolute rounded-full transition-all duration-[0.2s] ease-out scale-y-100 -translate-x-16 flex p-2 hover:p-3 scale-100 bg-green-400 hover:bg-green-600 text-white cursor-pointer'
 
-        next_button.className = 'absolute rounded-full transition-all duration-[0.2s] ease-out scale-x-100 -translate-y-16  flex p-3 bg-purple-600  text-white cursor-pointer'
+        next_button.className = 'absolute rounded-full transition-all duration-[0.2s] ease-out scale-x-100 -translate-y-16  flex p-2 hover:p-3 bg-purple-400 hover:bg-purple-600  text-white cursor-pointer'
 
-        previous_button.className = 'absolute rounded-full transition-all duration-[0.2s] ease-out scale-x-100 -translate-y-14 -translate-x-14   flex  p-3 bg-blue-600 text-white cursor-pointer'
+        previous_button.className = 'absolute rounded-full transition-all duration-[0.2s] ease-out scale-x-100 -translate-y-14 -translate-x-14   flex  p-2 hover:p-3 bg-blue-400 hover:bg-blue-600 text-white cursor-pointer'
     })
 
-    floating_button_container.addEventListener("focusout", () => {
-        const top_button = document.getElementById('top_button');
-        const next_button = document.getElementById('next_button');
-        const previous_button = document.getElementById('previous_button');
-
-        top_button.className = 'absolute rounded-full transition-all duration-[0.2s] ease-out scale-y-0 group-hover:scale-y-100 group-hover:-translate-x-16   flex  p-2 hover:p-3 bg-green-400 scale-100 hover:bg-green-600 text-white cursor-pointer'
-
-        next_button.className = 'absolute rounded-full transition-all duration-[0.2s] ease-out scale-x-0 group-hover:scale-x-100 group-hover:-translate-y-16  flex  p-2 hover:p-3 bg-purple-400 hover:bg-purple-600  text-white cursor-pointer'
-
-        previous_button.className = 'absolute rounded-full transition-all duration-[0.2s] ease-out scale-x-0 group-hover:scale-x-100 group-hover:-translate-y-14 group-hover:-translate-x-14   flex  p-2 hover:p-3 bg-blue-400 hover:bg-blue-600 text-white cursor-pointer'
-    })
+    // outside click
+    document.addEventListener('click', (event) => {
+        const floating_button_container = document.getElementById('floating_button');
+        if (!floating_button_container.contains(event.target)) {
+            show_floating_button();
+        }
+    });
 
     // top button event
     const top_button = document.getElementById('top_button');
